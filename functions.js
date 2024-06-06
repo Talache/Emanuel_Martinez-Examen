17a3e67b2b7b78445c37918296161386ed3392f5
Talache <31emanuel21@gmail.com>
2024-06-06 08:55:54 -0600

    5 LINEAS EN JS Y CSS

---
+++ functions.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const element = document.getElementsByName('h1');
+    element.textContent = 'UTCALVILLO!';
+    let nombre = 'Talache Martinez';
+    let edad = 22;
+});
