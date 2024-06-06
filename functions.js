40f9e855d335839f01ca90fff8358cfd75b58668
Talache <31emanuel21@gmail.com>
2024-06-06 09:01:11 -0600

    EDITAR EN FB EL JS

--- functions.js
+++ functions.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementsByName('h1');
     element.textContent = 'UTCALVILLO!';
     let nombre = 'Talache Martinez';
-    let edad = 22;
+
 });
